2d6ed4c29ccc0f96788f73089654af53796a788c
Aoihara <veronicadisa6@gmail.com>
2020-04-20 02:17:53 +0700

    ginkgo: Corvus-ROM

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/syberia_ginkgo.mk
+    $(LOCAL_DIR)/du_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    syberia_ginkgo-user \
-    syberia_ginkgo-userdebug \
-    syberia_ginkgo-eng
+    du_ginkgo-user \
+    du_ginkgo-userdebug \
+    du_ginkgo-eng
--- du_ginkgo.mk
+++ du_ginkgo.mk
@@ -7,19 +7,22 @@
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/syberia/common.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# PixelGApps
+$(call inherit-product, vendor/pixelgapps/pixel-gapps.mk)
 
 # Bootanimation
-TARGET_BOOT_ANIMATION_RES := 2160
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := syberia_ginkgo
+PRODUCT_NAME := du_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-SYBERIA_BUILD_TYPE := OFFICIAL
+DU_BUILD_TYPE := OFFICIAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ginkgo-user 9 PKQ1.190616.001 V10.3.3.0.PCOINXM release-keys" \
