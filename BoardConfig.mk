1d8a5b98d4f30db82fdea714bc1d58997eb491ee
TheHitMan7 <krtik.vrma@gmail.com>
2020-05-30 17:14:35 +0530

    ginkgo: Update vendor security patch level
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -214,7 +214,7 @@ TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 TARGET_USE_AOSP_SURFACEFLINGER := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-01-01
+VENDOR_SECURITY_PATCH := 2020-05-01
 
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
